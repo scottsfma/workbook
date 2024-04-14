9fc907023117c595397720575d86b88c8067cfaf
scottsfma <163093209+scottsfma@users.noreply.github.com>
2024-04-14 12:01:40 +1000

    Delete web_workbook directory

--- web_workbook/javaScript/menu.js
+++
@@ -1,8 +0,0 @@
-const menubutton = document.querySelector(".menubutton");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-menubutton.addEventListener("click", () => {
-  menubutton.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
